6daa3ff79509e92aa490fa329d6a524f6f01e990
seba <villeaho@tutanota.com>
2019-06-05 21:33:59 +0300

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,6 +18,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   	@user.microposts.paginate(page: 1).each do |micropost|
   		assert_match micropost.content, response.body
   	end
+    assert_select "div.stats"
+    assert_select "strong#followers", "#{@user.followers.count}"
+    assert_select "strong#following", "#{@user.following.count}"
   end
 
 
